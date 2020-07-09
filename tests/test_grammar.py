dd9ce46e98aa6c2e1045a9dfcd0eb76234c3921c
Pedro Medeiros <pedro.medeiros@gmail.com>
2020-07-09 13:43:28 -0300

    display .ASC file with exception caught

--- tests/test_grammar.py
+++ tests/test_grammar.py
@@ -50,17 +50,21 @@ class TestGrammar(unittest.TestCase):
         'test all .asc files in tests/samples against their respective .tokens file'
         test_files = glob(path.join('tests', 'samples', '[0-9][0-9].asc'))
         test_files.sort()
-        for filename in test_files:
+        for file_name in test_files:
             try:
-                with open(filename, 'r') as source_file:
+                with open(file_name, 'r') as source_file:
                     source_code = source_file.read()
-                    self.parse_file(filename) # looking for matching errors
-                with open(path.splitext(filename)[0] + '.tokens', 'r') as token_file:
+                    try:
+                        self.parse_file(file_name) # looking for matching errors
+                    except Exception as e:
+                        print('* exception captured in "%s"' % file_name, file=sys.stderr)
+                        raise
+                with open(path.splitext(file_name)[0] + '.tokens', 'r') as token_file:
                     tokens = token_file.read()
-                self.check_grammar(source_code, tokens, filename) # looking for comparison errors
+                self.check_grammar(source_code, tokens, file_name) # looking for comparison errors
             except FileNotFoundError as e:
-                with open(filename, 'r') as source_file:
+                with open(file_name, 'r') as source_file:
                     source_code = source_file.read()
                     result = '[%s]' % self.parse(source_code)
-                with open(path.splitext(filename)[0] + '.tokens', 'w') as token_file:
+                with open(path.splitext(file_name)[0] + '.tokens', 'w') as token_file:
                     print(result, file=token_file, end='') # generate .tokens file if it doesn't exist
