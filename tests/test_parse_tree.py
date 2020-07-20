dbcf66ac30874bf9221f17cc4c59877899b248bb
Pedro Medeiros <pedro.medeiros@gmail.com>
2020-07-20 18:10:50 -0300

    separation of sample test files in different directories.

--- tests/test_parse_tree.py
+++ tests/test_parse_tree.py
@@ -3,7 +3,6 @@ import sys
 import unittest
 import unittest.mock
 import arpeggio
-import pprint
 
 from os import path
 from glob import glob
@@ -12,6 +11,7 @@ from arpeggio import visit_parse_tree
 from lib import hitbasic
 from lib.visitor import MSXBasicVisitor
 from lib.language_statements import *
+from lib.exceptions import *
 
 
 class TestParseTree(unittest.TestCase):
@@ -22,65 +22,86 @@ class TestParseTree(unittest.TestCase):
         self.parser = hitbasic.create_parser()
 
 
-    def parse(self, text):
-        return self.parser.parse(text)
+    def visit(self, source):
+        'Generate symbol table and intermediate code from string.'
+        tree = self.parser.parse(source)
+        return visit_parse_tree(tree, MSXBasicVisitor(parser=self.parser, begin_line=10, debug=False))
 
 
-    def parse_file(self, filename):
-        'Generate symbol table and intermediate code.'
-        return self.parser.parse_file(filename)
-
-
-    def visit(self, tree):
+    def visit_file(self, file_name):
+        'Generate symbol table and intermediate code from file.'
+        tree = self.parser.parse_file(file_name)
         return visit_parse_tree(tree, MSXBasicVisitor(parser=self.parser, begin_line=10, debug=False))
 
 
     def test_visitor0(self):
         'An empty program is a valid program.'
-        tree = self.parse('')
-        symbol_table, code = self.visit(tree)
+        symbol_table, code = self.visit('')
         expected = [('@BeginProgram',), ('@EndProgram',), ('END',)]
         assert code == expected, 'got "%s" as result' % code
 
 
     def test_visitor1(self):
         text = """Print 1:Print 2"""
-        tree = self.parse(text)
-        symbol_table, code = self.visit(tree)
+        symbol_table, code = self.visit(text)
         expected = [('@BeginProgram',), ('PRINT', ' ', '1'), ('PRINT', ' ', '2'), ('@EndProgram',), ('END',)]
         assert code == expected, 'got "%s" as result' % code
 
 
+    def test_visitor1(self):
+        text = """Print 1:Print 2"""
+        symbol_table, code = self.visit(text)
+        expected = [('@BeginProgram',), ('PRINT', ' ', '1'), ('PRINT', ' ', '2'), ('@EndProgram',), ('END',)]
+        assert code == expected, 'got "%s" as result' % code
+
+
+    def test_visitor15(self):
+        source_file = './tests/samples/nodes/15_select_case.asc'
+        symbol_table, code = self.visit_file(source_file)
+        code = str().join(str(code).replace("' '", '␢').split())
+        with open(path.splitext(source_file)[0] + '.objdump', 'r') as obj_file:
+            expected = str().join(obj_file.read().replace("' '", '␢').split())
+            assert expected == code, 'expected: """\n%s\n""" in file "%s", got """\n%s\n""" instead' % (expected, source_file, code) # looking for comparison errors
+
+
     def test_parse_tree_in_files(self):
         'try to parse all .asc files in the nodes directory'
-        test_files = glob(path.join('tests', 'samples', '1??_*.asc'))
+        test_files = glob(path.join('tests', 'samples', 'nodes', '*.asc'))
         test_files.sort()
         for source_file in test_files:
-            s = io.StringIO()
-            tree = self.parse_file(source_file) # looking for matching errors
+            if source_file.count('.') > 1:
+                continue
             try:
-                symbol_table, code = self.visit(tree)
+                symbol_table, code = self.visit_file(source_file)
             except Exception as e:
                 print('* exception captured in "%s"' % source_file, file=sys.stderr)
                 raise
 
 
-    @unittest.skip("not implemented yet")
+    def test_exception_in_files(self):
+        'All *.<exceptionname>.asc files should trigger the respective exception.'
+        exception_map = {'typemismatch': TypeMismatch, 'loopnotfound': LoopNotFound}
+        for name in exception_map.keys():
+            test_files = glob(path.join('tests', 'samples', 'nodes', '*.%s.asc' % name))
+            test_files.sort()
+            for source_name in test_files:
+                with self.assertRaises(exception_map[name], msg=source_name):
+                    self.visit_file(source_name)
+
+
     def test_parse_tree_comparing_objdump(self):
         'try to parse all .asc files in the nodes directory and compare them with the respective objdump file'
-        test_files = glob(path.join('tests', 'samples', '1??_*.asc'))
+        test_files = glob(path.join('tests', 'samples', 'nodes', '*.asc'))
         test_files.sort()
         for source_file in test_files:
-            s = io.StringIO()
-            tree = self.parse_file(source_file) # looking for matching errors
-            symbol_table, code = self.visit(tree, filename=source_file)
-            pp = pprint.PrettyPrinter(stream=s, width=120)
-            pp.pprint(code)
-            code = s.getvalue().rstrip()
+            if source_file.count('.') > 1:
+                continue
             try:
+                symbol_table, code = self.visit_file(source_file)
+                code = str().join(str(code).replace("' '", '␢').split())
                 with open(path.splitext(source_file)[0] + '.objdump', 'r') as obj_file:
-                    obj_code = obj_file.read().rstrip()
-                    assert obj_code == code, 'expected: """\n%s\n""" in file "%s", got """\n%s\n""" instead' % (obj_code, source_file, code) # looking for comparison errors
+                    expected = str().join(obj_file.read().replace("' '", '␢').split())
+                    assert expected == code, 'expected: """\n%s\n""" in file "%s", got """\n%s\n""" instead' % (expected, source_file, code) # looking for comparison errors
             except FileNotFoundError as e:
                 with open(path.splitext(source_file)[0] + '.objdump', 'w') as obj_file:
                     print(code, file=obj_file)
